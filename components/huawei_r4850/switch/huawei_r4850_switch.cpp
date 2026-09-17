3d1a439d0a50af199d0ee1dac2711cbf0deef105
Sam Stenvall <sam.stenvall@nitor.com>
2026-09-17 12:07:03 +0300

    Hardcode assumed_state for switches
    https://developers.esphome.io/architecture/components/switch/#assumed-state-and-restore-mode
    
    > f your switch cannot read back the real hardware state (so the reported state is only what ESPHome last wrote), override assumed_state() to return true. The front-end will then show separate ON and OFF buttons rather than a single toggle.

--- components/huawei_r4850/switch/huawei_r4850_switch.cpp
+++ components/huawei_r4850/switch/huawei_r4850_switch.cpp
@@ -33,28 +33,23 @@ void HuaweiR4850Switch::send_state_(bool state) {
 }
 
 bool HuaweiR4850Switch::assumed_state() {
-  // if we could set the value to "unknown" we wouldn't need this, but we can't
-  // so we just tell HA we don't know the state.
-  return this->assumed_state_;
+  // Since we cannot poll the device for the actual state of the switch we
+  // have to use assumed state
+  return true;
 }
 
 void HuaweiR4850Switch::handle_update(uint16_t register_id, std::vector<uint8_t> &data) {
   if (register_id != this->registerId_)
     return;
   this->publish_state(data[1]);
-  this->assumed_state_ = false;
 }
+
 void HuaweiR4850Switch::handle_error(uint16_t register_id, std::vector<uint8_t> &data) {
-  if (register_id != this->registerId_)
-    return;
-  // we should set the state to "unknown" here, but ESPHome doesn't have a way to do that (for switch entities).
-  this->assumed_state_ = true;
+
 }
+
 void HuaweiR4850Switch::handle_timeout() {
-  // we should set the state to "unavailable" here, but ESPHome doesn't have a way to do that:
-  // https://github.com/esphome/feature-requests/issues/1568
-  // and there isn't a way to set it to "unknown" either.
-  this->assumed_state_ = true;
+
 }
 
 }  // namespace huawei_r4850
--- components/huawei_r4850/switch/huawei_r4850_switch.h
+++ components/huawei_r4850/switch/huawei_r4850_switch.h
@@ -29,8 +29,6 @@ class HuaweiR4850Switch : public switch_::Switch, public Component, public Huawe
   std::optional<bool> last_state_;
   bool restore_value_{true};
 
-  bool assumed_state_{true};
-
   void send_state_(bool value);
 
   void write_state(bool state) override;
